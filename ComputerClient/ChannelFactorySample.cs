4ff987372d19ee7b242475ee3b5751f086716b29
Rajesh <rajhseg@gmail.com>
2024-03-23 13:13:14 +0530

    Update ChannelFactorySample.cs

--- ComputerClient/ChannelFactorySample.cs
+++ ComputerClient/ChannelFactorySample.cs
@@ -13,51 +13,83 @@ namespace ComputerClient
     	ChannelFactory<CalculatorLibrary.ICalculatorService> factory = null;
     	ICalculatorService channel = null;
     	
+    	DuplexChannelFactory<CalculatorLibrary.IFileService> factory2 = null;
+    	IFileService channel2 = null;
+    	
     	public ChannelFactorySample()
     	{
+    		// Channel Factory
     		factory = new ChannelFactory<CalculatorLibrary.ICalculatorService>(
-	              new WSHttpBinding(), new EndpointAddress("http://localhost:8082/ComService"));
+	              new WSHttpBinding(), new EndpointAddress("http://localhost:8082/ComService"));    		
+    		channel = factory.CreateChannel();
+    		((IClientChannel)channel).Faulted+= new EventHandler(channel_Faulted);
+    		
+    		
+    		// Duplex channel factory
+    		InstanceContext context = new InstanceContext(this);
+                var binding = new NetTcpBinding();
+                binding.Security.Mode = SecurityMode.None;
+                
+            factory2 = new DuplexChannelFactory<CalculatorLibrary.IFileService>(context,
+                    binding, new EndpointAddress("net.tcp://localhost:8090/ComService"));
+
+            channel2 = factory2.CreateChannel();
+            ((IClientChannel)channel2).Faulted += Program_Faulted;
     	}
+
+    	
+    	void channel_Faulted(object sender, EventArgs e)
+    	{
+    		var closeChannel = (ICommunicationObject) channel;
+    		
+    		if(closeChannel!=null)
+    			closeChannel.Close();
+    		
+    		factory = new ChannelFactory<CalculatorLibrary.ICalculatorService>(
+	              		new WSHttpBinding(), new EndpointAddress("http://localhost:8082/ComService"));
+            		
+            channel = factory.CreateChannel();
+            ((IClientChannel)channel).Faulted+= new EventHandler(channel_Faulted);
+    	}
+    	
+    	
+        private void Program_Faulted(object sender, EventArgs e)
+        {
+                		
+        	var closeChannel = (ICommunicationObject) channel2;
+    		
+    		if(closeChannel!=null)
+    			closeChannel.Close();
+    		
+    		InstanceContext context = new InstanceContext(this);
+                var binding = new NetTcpBinding();
+                binding.Security.Mode = SecurityMode.None;
+                
+            
+            factory2 = new DuplexChannelFactory<CalculatorLibrary.IFileService>(context,
+                    binding, new EndpointAddress("net.tcp://localhost:8090/ComService"));
+
+            channel2 = factory2.CreateChannel();
+			((IClientChannel)channel2).Faulted += Program_Faulted;
+        }
+
     	
         public void Process()
         {
-            /* Channel Factory */
-			
-            try {
-	
-	            channel = factory.CreateChannel();
+            /* Channel Factory */			
+            try {		            
 	            var result = channel.Add(new CalculatorLibrary.CalRequest { A = 20, B = 2, Key = "sssddd" });	
 	            Console.WriteLine(result.Result);
             }
-            catch(System.ServiceModel.CommunicationException ex){
-            	            	            		
-            	if(factory.State == CommunicationState.Faulted){
-            		
-            		 factory = new ChannelFactory<CalculatorLibrary.ICalculatorService>(
-	              		new WSHttpBinding(), new EndpointAddress("http://localhost:8082/ComService"));
-            	}
+            catch(FaultException ex){
             	
-            } catch(Exception ex){
-            	
-            	 factory = new ChannelFactory<CalculatorLibrary.ICalculatorService>(
-	              new WSHttpBinding(), new EndpointAddress("http://localhost:8082/ComService"));
-            	
+            	Console.WriteLine(ex.Message);
             }
 
             try
             {  
                 /* Duplex channel Factory */
-                InstanceContext context = new InstanceContext(this);
-                var binding = new NetTcpBinding();
-                binding.Security.Mode = SecurityMode.None;
-
-                DuplexChannelFactory<CalculatorLibrary.IFileService> factory2 = new DuplexChannelFactory<CalculatorLibrary.IFileService>(context,
-                    binding, new EndpointAddress("net.tcp://localhost:8090/ComService"));
-
-                var channel2 = factory2.CreateChannel();
-                channel2.StartProcess(new Employee { EmployeeID = 1, Job = "AA", Name = "SD" });
-
-                ((IClientChannel)channel2).Faulted += Program_Faulted;
+                channel2.StartProcess(new Employee { EmployeeID = 1, Job = "AA", Name = "SD" });                
             }
             catch (FaultException<CalculatorLibrary.CalFaultMessage> mes)
             {
@@ -65,12 +97,7 @@ namespace ComputerClient
             }
             Console.Read();
         }
-
-        private static void Program_Faulted(object sender, EventArgs e)
-        {
-            Console.WriteLine("error");
-        }
-
+        
         /* Callback Method from server */
         public void Progress(byte[] data)
         {
